b4186ed32f7ffe940642d40a9faded6a90b3fa1d
Marakarn! <suttikarn1997@gmail.com>
2024-01-09 19:01:49 +0700

    finish_assessment_edit_owner_pic_3nd

--- src/Owner.jsx
+++ src/Owner.jsx
@@ -6,10 +6,10 @@ const Owner = () => {
     return (
     <Layout>
         <h1>Karn - Group B - 27</h1>
-        <img className='owner_pic' src={"public/20651e.jpg"} alt="owner_picture" />
+        <img className='owner_pic' src="./20651e.jpg" alt="owner_picture" />
         <p>Short Biography:</p>
         <p className='bio_text'>I am a Student from Generation's Junior Software Developer. I have 2 years of experience in Graphic Design.  My passion for technology has driven me to explore web development and Cloud computing. I am determined to pursue a career as a Front-end Software Developer. Drawing on my background in Graphic Design, I aim to enhance website development with a keen eye for aesthetics and user experience.</p>
-        <img className='coffee_pic' src={"public/HotCoffee.gif"} alt="coffee_picture" />
+        <img className='coffee_pic' src="./HotCoffee.gif" alt="coffee_picture" />
     </Layout>
     )
 }
